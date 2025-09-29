c7dbc62bd51234cbe219ea5d60433ad4931d0ca5
aarbizu-2020398 <aarbizu-2020398@kinal.edu.gt>
2025-09-29 07:10:28 -0600

    Se implementaron mas cosas especificas en el backend como, publicaciones, comentarios, se mejoraron las funcionalidades del login, register,

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 import app from './configs/server.js';
 
 config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
 	console.log(`Servidor de encuestas escuchando en el puerto ${PORT}`);
 });
